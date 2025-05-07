fb997a64d23d171923858f625ce66160b950def4
Oskar Świda <oskar.swida@gmail.com>
2025-05-07 23:35:14 +0200

    golden files tests

--- go.mod
+++ go.mod
@@ -4,4 +4,11 @@ go 1.24.0
 
 require github.com/alecthomas/participle/v2 v2.1.4
 
-require github.com/shopspring/decimal v1.4.0 // indirect
+require (
+	github.com/alecthomas/assert/v2 v2.11.0 // indirect
+	github.com/alecthomas/repr v0.4.0 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/hexops/gotextdiff v1.0.3 // indirect
+	github.com/shopspring/decimal v1.4.0 // indirect
+	gotest.tools/v3 v3.5.2 // indirect
+)
---
+++ parser/parser_test.go
@@ -0,0 +1,32 @@
+package parser_test
+
+import (
+	"encoding/json"
+	"mt9x/grammar"
+	"mt9x/parser"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/alecthomas/assert/v2"
+	"gotest.tools/v3/golden"
+)
+
+func TestProperMT940Files(t *testing.T) {
+	parser := parser.NewFileParser[grammar.MT940Message]()
+	basePath := filepath.Join("testdata", "mt940")
+	files, err := os.ReadDir(filepath.Join(basePath, "input"))
+	assert.NoError(t, err)
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		result, err := parser.Parse(filepath.Join(basePath, "input", f.Name()), nil)
+		assert.NoError(t, err)
+		value, err := json.MarshalIndent(result, "", " ")
+		assert.NoError(t, err)
+		golden.AssertBytes(t, value, filepath.Join("mt940", "expected", strings.ReplaceAll(f.Name(), ".msg", ".json")))
+	}
+
+}
