c476f8cc3b7e8ed23043d755f9349fe9d9c68b49
Oskar Swida <oskar.swida.external@worldline.com>
2025-07-18 14:33:05 +0200

    byte parser

--- parser/parser.go
+++ parser/parser.go
@@ -45,3 +45,37 @@ func (fp *FileParser[T]) Parse(filename string, validate bool, traceWriter io.Wr
 
 	return res, nil
 }
+
+type ByteParser[T MT9xMessage] struct {
+	parser *participle.Parser[T]
+}
+
+// NewByteParser creates new byte parser for MT940 messages.
+func NewByteParser[T MT9xMessage]() *ByteParser[T] {
+	lexer := NewLexer()
+	parser := participle.MustBuild[T](
+		participle.Lexer(lexer),
+		participle.UseLookahead(2))
+	return &ByteParser[T]{
+		parser: parser,
+	}
+}
+
+// Parse parses MT940 message (from data) into structure.
+func (fp *ByteParser[T]) Parse(data []byte, validate bool, traceWriter io.Writer) (*T, error) {
+	options := []participle.ParseOption{participle.AllowTrailing(true)}
+	if traceWriter != nil {
+		options = append(options, participle.Trace(traceWriter))
+	}
+	res, err := fp.parser.ParseBytes("byte data", data, options...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse bytes: %w", err)
+	}
+	if validate {
+		if err = (*res).Validate(); err != nil {
+			return nil, fmt.Errorf("failed to validate parsed result: %w", err)
+		}
+	}
+
+	return res, nil
+}
